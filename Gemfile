0992fb860c10c31ce096ab0bdae445d10e46d296
hassanse91 <senhajihassan10@gmail.com>
2016-08-11 19:40:01 +0000

    add user_id to post

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'devise'
 gem "paperclip", "~> 5.0.0"
+gem 'devise-bootstrap-views'
+
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811192136) do
+ActiveRecord::Schema.define(version: 20160811193343) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160811192136) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
